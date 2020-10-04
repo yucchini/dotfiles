a6fdd7210a9a892d2b02997841532362c77df0bd
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2020-10-04 14:32:57 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/
-ln -sf ~/dotfiles/
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/
 ln -sf ~/dotfiles/
 ln -sf ~/dotfiles/
