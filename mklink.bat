4c3e5725812ce069c7bb423e13603015cfe35af8
Yuki Shimizu <yucchini@yucchini-Macbook-pro.local>
2021-05-02 10:47:50 +0900

    Remove unused files

--- mklink.bat
+++
@@ -1,4 +0,0 @@
-mklnk %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklnk %HOMEPATH%"\.tmux.conf" %HOMEPATH%"\dotfiles\.tmux.conf"
-mklnk %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
-exit 0
