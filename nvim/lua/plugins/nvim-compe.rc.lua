b1e96f24206048cbabcadf588133203c9a57d381
uki <yucchini.yuki1014@gmail.com>
2021-07-04 01:00:19 +0900

    Add lspconfig and lspsaga config

---
+++ nvim/lua/plugins/nvim-compe.rc.lua
@@ -0,0 +1,42 @@
+vim.o.completeopt = "menuone,noselect"
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    spell = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+  };
+}
+vim.api.nvim_command([[
+  inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+  inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+  inoremap <silent><expr> <C-Space> compe#complete()
+  inoremap <silent><expr> <CR>      compe#confirm("<CR>")
+]])
+
+-- lspsaga
+--local saga = require 'lspsaga'
+--saga.init_lsp_saga()
+
+--vim.api.nvim_set_keymap('n', 'ga', ':Lspsaga code_action<CR>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('v', 'ga', ':Lspsaga range_code_action<CR>', { noremap = true, silent = true })
+
+--vim.api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
+
+--vim.api.nvim_set_keymap('n', 'gr', ':Lspsaga rename<CR>', { noremap = true, silent = true })
